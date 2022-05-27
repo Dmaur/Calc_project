5cedb314649cf78b77b31235ff584173b4ac85c2
Derrick <derrickmaurais@gmail.com>
2022-05-26 21:11:50 -0300

    all the buttons have usable functionality

--- calc.js
+++ calc.js
@@ -1,1 +1,31 @@
+// capture the number input
+let num = document.querySelectorAll(".numbers");
+// num.forEach((number) => {
+//   number.addEventListener(onmouseover, function () {});
+// }); // this does not work yet.
+num.forEach((number) => {
+  number.addEventListener("click", function () {
+    document.getElementById("display").innerHTML =
+      "you pressed number " + number.innerHTML;
+  });
+});
 
+//clear the display screen.
+let clear = document.getElementById("clear");
+clear.addEventListener("click", function () {
+  document.getElementById("display").innerHTML = "";
+});
+
+// capture numerator button input
+let numerators = document.querySelectorAll(".numerators");
+numerators.forEach((numerator) => {
+  numerator.addEventListener("click", function () {
+    document.getElementById("display").innerHTML = numerator.innerHTML;
+  });
+});
+
+// capture delete button input
+let Del = document.getElementById("Del");
+Del.addEventListener("click", function () {
+  document.getElementById("display").innerHTML = Del.innerHTML;
+});
